30d5f98d13aaa54da1949597493c439e4877e628
徐大帅 <xudashuai0819@outlook.com>
2018-03-06 00:13:27 +0800

    0.1版本

--- src/store/modules/zb.js
+++ src/store/modules/zb.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8081/';
+axios.defaults.baseURL = 'http://120.25.75.23:8081/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 function zbDataCreate () {
